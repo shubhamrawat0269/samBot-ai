47cab9b13200955b11907fb040fa040af5529cce
shubham Rawat <rawatshubam269@gmail.com>
2024-04-09 19:14:24 +0530

    pushing local files to remote:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+import Workspace from "./components/Workspace";
+
+export function App() {
+  return (
+    <div className="">
+      {/* Sidebar  */}
+      <Sidebar />
+      {/* Workspace  */}
+      <Workspace />
+    </div>
+  );
+}
