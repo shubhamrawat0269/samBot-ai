572d5244fb25dd9cd081e6c0892b0942cc7b9161
shubham Rawat <rawatshubam269@gmail.com>
2024-04-10 12:56:09 +0530

    Comment out openai.js file for a temp time

--- src/openai.js
+++ src/openai.js
@@ -1,19 +1,19 @@
-const { Configuration, OpenAIApi } = require("openai");
-// import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-  apiKey: "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",
-});
-const openai = new OpenAIApi(configuration);
+// const { Configuration, OpenAIApi } = require("openai");
+// // import { Configuration, OpenAIApi } from "openai";
+// const configuration = new Configuration({
+//   apiKey: "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",
+// });
+// const openai = new OpenAIApi(configuration);
 
-export async function sendMsgToOpenAI(message) {
-  const res = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: message,
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presense_penalty: 0,
-  });
-  return res.data.choices[0].text;
-}
+// export async function sendMsgToOpenAI(message) {
+//   const res = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: message,
+//     temperature: 0.7,
+//     max_tokens: 256,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     presense_penalty: 0,
+//   });
+//   return res.data.choices[0].text;
+// }
